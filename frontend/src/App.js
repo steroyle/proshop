0802393cef12d301fc48ce472042277266f1366e
steroyle <steroyle@gmail.com>
2023-06-25 14:56:27 +0100

    Added react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
-import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to ProShop</h1>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
