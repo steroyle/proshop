b844ee7699c5ca865c3a433388e6693c86b8b44f
steroyle <steroyle@gmail.com>
2023-07-19 23:17:36 +0100

    Fixed a broken link

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -5,7 +5,7 @@ import Rating from "./Rating";
 const Product = ({product}) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Link href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
 
