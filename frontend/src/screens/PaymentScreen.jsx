a4d9830db5f803551bbaa92f48943a8bddaf70c1
steroyle <steroyle@gmail.com>
2023-08-13 21:29:33 +0100

    Added payment screen

---
+++ frontend/src/screens/PaymentScreen.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { Button, Col, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import CheckoutSteps from '../components/CheckoutSteps';
+import FormContainer from '../components/FormContainer';
+import { savePaymentMethod } from '../slices/cartSlice';
+
+const PaymentScreen = () => {
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate('/placeorder');
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='address' className='my-2'>
+          <Form.Label>Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type='radio'
+              className='my-2'
+              label='PayPal or Credit Card'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type='submit' variant='primary' className='my-2'>Continue</Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default PaymentScreen;
