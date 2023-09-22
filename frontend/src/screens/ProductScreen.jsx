f0be5f89d8c30b0a743faf66201d55a68aad1c85
steroyle <steroyle@gmail.com>
2023-09-23 00:14:28 +0100

    Removed helmet meta component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -8,7 +8,7 @@ import { useGetProductDetailsQuery, useCreateReviewMutation } from "../slices/pr
 import { addToCart } from "../slices/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import Meta from '../components/Meta';
+// import Meta from '../components/Meta';
 
 const ProductScreen = () => {
 
@@ -55,7 +55,7 @@ const ProductScreen = () => {
         error ? (<Message variant="danger">{ error?.data?.message || error.error }</Message>) :
         (
           <>            
-            <Meta title={product.name} description={product.description} />
+            {/* <Meta title={product.name} description={product.description} /> */}
             <Row>
               <Col md={5}>
                 <Image src={product.image} alt={product.name} fluid />
